dcb32a3ad84ead41c8e13d5e2adcce03fb117f1e
AITBELLAouss <ouss.chute@gmail.com>
2022-05-11 21:40:23 +0200

    finale touches

--- app/src/main/java/com/example/movierec/MainActivity.java
+++ app/src/main/java/com/example/movierec/MainActivity.java
@@ -17,8 +17,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
+
 import java.util.ArrayList;
 import java.util.List;
 
